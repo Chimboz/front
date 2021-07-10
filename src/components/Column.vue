f826f3eebd53520608ef6508a10ae1f2991e0d53
Tigriz <tigriz@tfwno.gf>
2021-07-10 23:55:24 +0200

    Column

---
+++ src/components/Column.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <img
+      class="card-header-img"
+      v-if="filename"
+      :src="require(`@/assets/img/${filename}`)"
+    />
+    <div class="card">
+      <h2>
+        <slot name="header"></slot>
+      </h2>
+      <em>"<slot name="subtitle"></slot>"</em>
+      <br />
+      <br />
+      <main>
+        <slot></slot>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    bot: {
+      type: Boolean,
+      default: false
+    },
+    top: {
+      type: Boolean,
+      default: false
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: 12px;
+  width: 154px;
+  padding: 17px 5px;
+  height: auto;
+  text-align: center;
+  background-color: #d5e6f3;
+  background-image: linear-gradient(
+    to bottom,
+    #aadcfc 0px,
+    #6ebef0 12px,
+    #6ebef0 calc(100% - 12px),
+    #5aa1cd 100%
+  );
+}
+
+img {
+  display: block;
+}
+
+img + .card {
+  border-radius: 0 0 12px 12px;
+  display: inline-block;
+  background-image: linear-gradient(
+    to bottom,
+    #6ebef0 0px,
+    #6ebef0 calc(100% - 12px),
+    #5aa1cd 100%
+  );
+}
+</style>
