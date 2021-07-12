cc95edfbca7ad206b644c44cfee47a78ae40d006
Tigriz <tigriz@tfwno.gf>
2021-07-12 19:09:01 +0200

    Navbar css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="header">
-    <router-link to="/"><img id="logo" src="../assets/img/logo.svg" /></router-link>
+    <router-link to="/"
+      ><img id="logo" src="../assets/img/logo.svg"
+    /></router-link>
     <div class="toast">
       <div style="margin-top: -10px">
         <p id="username">{{ username }}</p>
@@ -12,12 +14,18 @@
     </div>
   </div>
   <div id="nav">
-    <router-link to="/">Accueil</router-link>
-    <router-link to="/tchat">Tchat</router-link>
-    <router-link to="/account">Mon compte</router-link>
-    <router-link to="/games">Jeux</router-link>
-    <router-link to="/members">Membres</router-link>
-    <router-link to="/bbs">Forum</router-link>
+    <router-link to="/"><button class="nav-btn">Accueil</button></router-link>
+    <router-link to="/tchat"
+      ><button class="nav-btn">Jouer !</button></router-link
+    >
+    <router-link to="/account"
+      ><button class="nav-btn">Mon compte</button></router-link
+    >
+    <router-link to="/games"><button class="nav-btn">Jeux</button></router-link>
+    <router-link to="/members"
+      ><button class="nav-btn">Membres</button></router-link
+    >
+    <router-link to="/bbs"><button class="nav-btn">Forum</button></router-link>
   </div>
 </template>
 
@@ -34,33 +42,12 @@ export default {
   },
   components: {
     Tiz,
-  }
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-#nav a {
-  text-decoration: none;
-  padding: 10px;
-  font-size: 15px;
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a:hover {
-  color: #2c3e50aa
-}
-
-#nav {
-  text-align: center;
-  padding: 10px;
-}
-
-#nav a.router-link-exact-active {
-  color: #fff;
-}
-
 .header {
   width: 100%;
   height: 70px;
@@ -109,5 +96,58 @@ export default {
   background: #ade5f3;
   font-weight: bold;
   letter-spacing: 0;
+}
+
+.nav-btn {
+  cursor: pointer;
+  height: 25px;
+  font-family: "Chimboz Heavy";
+  font-weight: normal;
+  color: #fff;
+  background-image: linear-gradient(
+    to bottom,
+    #ffe4f5 0%,
+    #ff1fb0 50%,
+    #b30048 50%,
+    #f0009c 100%
+  );
+  padding: 5px 20px;
+  font-size: 17px;
+  color: #fff;
+  border-width: 1px;
+  border-style: solid;
+  border-image: linear-gradient(to bottom, #f0009c, #b40075) 1 100%;
+}
+
+#nav a:first-child .nav-btn {
+  border-radius: 10px 0 0 10px;
+}
+
+#nav a:last-child .nav-btn {
+  border-radius: 0 10px 10px 0;
+}
+
+.nav-btn:hover {
+  background-image: linear-gradient(
+    to bottom,
+    #fff2ea 0%,
+    #ff7214 50%,
+    #ce4800 50%,
+    #ff6600 100%
+  );
+  border-image: linear-gradient(to bottom, #ff6600, #973500) 1 100%;
+}
+
+.nav-btn a:hover {
+  text-decoration: none;
+}
+
+#nav {
+  text-align: center;
+  padding-bottom: 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: #fff;
 }
 </style>
