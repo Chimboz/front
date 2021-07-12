731f117c39b693e058197f4094cc7c1adfde9a11
Tigriz <tigriz@tfwno.gf>
2021-07-13 01:02:02 +0200

    Testing addition of loadtime

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    CHAPATIZ® | Infos légales | Parents | Règles d'or | Protection de l'enfance | Sécurité | Ethique
+Conditions générales d'utilisation | Conditions générales de vente
+Partenaires : La Cartoonerie | EnjoyStation Webradio
+[0.74s] [b]
+</template>
