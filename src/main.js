e6f8541566744dc35c6197595e199149fe03ecf2
Tigriz <tigriz@tfwno.gf>
2021-07-18 16:17:04 +0200

    Adding vuex and axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from "./App.vue";
 import "./assets/scss/fonts.css";
 import "./assets/scss/main.scss";
 import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import Vuex from "vuex";
 
 createApp(App)
   .use(router)
+  .use(VueAxios, axios)
+  .use(Vuex)
   .mount("#app");
