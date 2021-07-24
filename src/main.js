f0c23302f3382f2dd7042266b0f72a742b935924
unknown <yanisse.fekraoui@gmail.com>
2021-07-24 13:45:13 +0200

    Translation added : based on the browser's language

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import Vuex from "vuex";
+import i18n from './i18n'
 
-createApp(App)
+createApp(App).use(i18n)
   .use(router)
   .use(VueAxios, axios)
   .use(Vuex)
