7d4a7243c9d2c8d761a50b11a52f9d8e76181014
Tigriz <tigriz@tfwno.gf>
2021-07-25 22:43:34 +0200

    Members, Rules, BBS

---
+++ src/components/Rules.vue
@@ -0,0 +1,18 @@
+<template>
+    <Card blue bot>Ne donne jamais ton mot de passe, à personne&nbsp;!<br><Button>Règles</Button></Card>
+</template>
+<script>
+import Card from "@/components/Card.vue";
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "Rules",
+  components: {
+    Card,
+    Button,
+  },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
