b49d508ab5a9574cd15ec4c32b76651066752677
Tigriz <tigriz@tfwno.gf>
2021-08-03 08:51:23 +0200

    ProgressBar, mitt, lodash

--- src/main.js
+++ src/main.js
@@ -6,11 +6,31 @@ import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import store from "./store/index.js";
+import mitt from 'mitt';
 import i18n from "./i18n";
 
-createApp(App)
+const eventBus = mitt()
+const app = createApp(App)
   .use(i18n)
   .use(router)
   .use(VueAxios, axios)
   .use(store)
-  .mount("#app");
+
+app.config.globalProperties.eventBus = eventBus
+
+app.mount("#app");
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  if (typeof to.matched[0]?.components.default === "function") {
+    eventBus.emit("asyncComponentLoading", to); // Start progress bar
+  }
+  next();
+});
+
+router.beforeResolve((to, from, next) => {
+  eventBus.emit("asyncComponentLoaded"); // Stop progress bar
+  next();
+});
+
+export default app;
--- vue.config.js
+++ vue.config.js
@@ -25,10 +25,10 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'fr',
-      fallbackLocale: 'fr',
-      localeDir: 'locales',
-      enableInSFC: true
-    }
+      locale: "fr",
+      fallbackLocale: "fr",
+      localeDir: "locales",
+      enableInSFC: true,
+    },
   }
-}
+};
