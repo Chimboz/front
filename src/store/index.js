e69f2a37ca154815e22cb8ce19c24d1d8db0e44d
Taigah <taigah.dev@gmail.com>
2021-09-07 22:01:42 +0200

    setup base auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,24 @@
 import { createStore } from "vuex";
+import auth from './modules/auth.js';
 
 const store = createStore({
-    state: {
-      progress: 10,
+  state: {
+    progress: 10,
+  },
+  mutations: {
+    start(state) {
+      state.progress = 10;
     },
-    mutations: {
-      start(state) {
-        state.progress = 10;
-      },
-      increment(state) {
-        state.progress++;
-      },
-      done(state) {
-        state.progress = 100;
-      },
+    increment(state) {
+      state.progress++;
     },
+    done(state) {
+      state.progress = 100;
+    },
+  },
+  modules: {
+    auth
+  }
 });
 
 export default store
