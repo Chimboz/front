367352577e41e2ce42f243dc5b201cc6bd700990
Tigriz <tigriz@tfwno.gf>
2021-09-08 14:05:07 +0200

    Example user

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,26 +1,41 @@
 export default {
   namespaced: true,
-  state () {
+  state() {
     return {
-      user: null
-    }
+      user: null,
+    };
   },
   getters: {
     authenticated(state) {
-      return state.user !== null
-    }
+      return state.user !== null;
+    },
   },
   mutations: {
-    setUser (state, user) {
-      state.user = user
-    }
+    setUser(state, user) {
+      state.user = user;
+    },
   },
   actions: {
     login({ commit }) {
-      commit("setUser", { "name": "Tigriz", "id": 3})
+      commit("setUser", {
+        name: "Tigriz",
+        id: 3,
+        color: "#900",
+        role: 100,
+        look: {
+          avatar: 0,
+          emote: 1,
+          hat: 7,
+          body: 17,
+          shoe: 7,
+          item0: 17,
+          item1: 1,
+          item2: 1,
+        },
+      });
     },
-    logout ({ commit }) {
-      commit("setUser", null)
-    }
-  }
-}
+    logout({ commit }) {
+      commit("setUser", null);
+    },
+  },
+};
