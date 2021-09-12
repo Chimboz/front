f756256022b46f760ee60817fbd333c09a449ca4
Tigriz <tigriz@tfwno.gf>
2021-09-12 20:54:37 +0200

    Modal

--- src/component/Modal.vue
+++ src/component/Modal.vue
@@ -1,8 +1,41 @@
 <template>
-    <div class="modal"></div>
+  <div class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+      <img
+        style="float: left"
+        draggable="false"
+        @contextmenu.prevent
+        src="@/asset/img/icon/warning_modal.svg"
+      />
+      <p>Some text in the Modal..</p>
+      <button class="pink-icon">
+        ok
+      </button>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
 .modal {
-    position: absolute
+  position: fixed;
+  z-index: 3;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: #0007;
+}
+
+.modal-content {
+  background-color: #69c;
+  margin: 15% auto;
+  padding: 20px;
+  color: #fff;
+  border: 2px solid;
+  border-color: #7fbcff #184672 #184672 #7fbcff;
+  border-radius: 6px;
+  width: 362px;
+  height: 103px;
 }
 </style>
