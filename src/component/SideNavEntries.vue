bd34da13dc0f495e43720348826a85e92753f1c9
Tigriz <tigriz@tfwno.gf>
2021-09-18 17:40:10 +0200

    Adding devlog and documentation

--- src/component/SideNavEntries.vue
+++ src/component/SideNavEntries.vue
@@ -23,110 +23,120 @@ import { defineProps } from "vue";
 const props = defineProps({
   section: {
     required: true,
-    type: String,
-  },
+    type: String
+  }
 });
 
 const sideNavEntries = [
   {
     label: "section.account",
     url: "/account",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.messenger",
     url: "/messenger",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.groups",
     url: "/groups",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.friends",
     url: "/friends",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.forum",
     url: "/bbs",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.password",
     url: "/password",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.capitalization",
     url: "/majmin",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.shop",
     url: "/shop",
-    section: "account",
+    section: "account"
   },
   {
     label: "section.games",
     url: "/games",
-    section: "games",
+    section: "games"
   },
   {
     label: "section.bacteria",
     url: "/games/bacteria",
-    section: "games",
+    section: "games"
   },
   {
     label: "section.patojdur",
     url: "/games/patojdur",
-    section: "games",
+    section: "games"
   },
   {
     label: "section.mazo",
     url: "/games/mazo",
-    section: "games",
+    section: "games"
   },
   {
     label: "section.members",
     url: "/members",
-    section: "members",
+    section: "members"
   },
   {
     label: "section.book",
     url: "/book",
-    section: "members",
+    section: "members"
   },
   {
     label: "section.popularity",
     url: "/popularity",
-    section: "members",
+    section: "members"
   },
   {
     label: "section.wedding",
     url: "/wedding",
-    section: "members",
+    section: "members"
   },
   {
     label: "section.groups",
     url: "/groups",
-    section: "members",
+    section: "members"
   },
   {
     label: "section.forum",
     url: "/bbs",
-    section: "community",
+    section: "community"
   },
   {
     label: "section.shop",
     url: "/shop",
-    section: "community",
+    section: "community"
   },
   {
     label: "section.encyclopedia",
     url: "/enclyclopedia",
-    section: "community",
+    section: "community"
   },
+  {
+    label: "section.devlog",
+    url: "/devlog",
+    section: "community"
+  },
+  {
+    label: "section.documentation",
+    url: "/documentation",
+    section: "community"
+  }
 ];
 </script>
