40e38dfa058e1d9516ae33fc88fd2dcbc8c0332a
Tigriz <tigriz@tfwno.gf>
2021-09-18 15:59:33 +0200

    type=button on Emotes

--- src/component/Emotes.vue
+++ src/component/Emotes.vue
@@ -1,181 +1,181 @@
 <template>
   <div>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/default.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/yo.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/lol.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/sad.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/cry.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/angry.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/wink.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/korbak.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/tongue.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/luv.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/oh.svg"
       />
     </button>
-    <button class="resize pink-icon">
+    <button type="button" class="resize pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/zzz.svg"
       />
     </button>
-    <button class="resize pink-icon">
+    <button type="button" class="resize pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/flag.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/neko.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/irritated.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/left.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/focused.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/vv.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/shameful.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/oops.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/kawai.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/parano.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/crazy.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/fearful.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
         src="@/asset/img/icon/emote/smirk.svg"
       />
     </button>
-    <button class="pink-icon">
+    <button type="button" class="pink-icon">
       <img
         draggable="false"
         @contextmenu.prevent
