2ba2fa71c13de1e5efad551b4c60007639268b3f
Tigriz <tigriz@tfwno.gf>
2021-11-05 21:38:44 +0100

    Pagination

--- src/component/bbs/Pagination.vue
+++ src/component/bbs/Pagination.vue
@@ -1,3 +1,16 @@
 <template>
-  <div></div>
+  <router-link
+    :to="'/topic/' + $route.params.id + '/' + $route.params.topic + '/1'"
+    class="pink"
+    >Page 1</router-link
+  >
+  <router-link
+    :to="'/topic/' + $route.params.id + '/' + $route.params.topic + '/2'"
+    class="pink"
+    >Page 2</router-link
+  >
 </template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
--- src/component/bbs/list/Topic.vue
+++ src/component/bbs/list/Topic.vue
@@ -21,20 +21,7 @@
     <thead>
       <tr>
         <th valign="top" colspan="2" height="25" nowrap="nowrap">
-          <router-link
-            :to="
-              '/topic/' + $route.params.id + '/' + $route.params.topic + '/1'
-            "
-            class="pink"
-            >Page 1</router-link
-          >
-          <router-link
-            :to="
-              '/topic/' + $route.params.id + '/' + $route.params.topic + '/2'
-            "
-            class="pink"
-            >Page 2</router-link
-          >
+          <pagination />
         </th>
       </tr>
     </thead>
@@ -46,16 +33,23 @@
         :separator="index != messages.length - 1"
       />
     </tbody>
+    <tr>
+      <th valign="bottom" colspan="2" height="25" nowrap="nowrap">
+        <pagination />
+      </th>
+    </tr>
   </table>
 </template>
 
 <script>
+import Pagination from "../Pagination.vue";
 import Message from "../row/Message.vue";
 
 export default {
   name: "Topic List",
   components: {
-    Message
+    Message,
+    Pagination
   },
   props: {
     messages: {
