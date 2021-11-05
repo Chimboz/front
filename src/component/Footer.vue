d19821826e4c358a34ed984de82743258971359a
Tigriz <tigriz@tfwno.gf>
2021-11-05 17:41:40 +0100

    New Twitter

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -2,13 +2,13 @@
   <footer>
     <hr />
     <br />
-    <a href="https://twitter.com/ChapatizRetro" target="_blank"
+    <a href="https://twitter.com/ChimbozOfficial" target="_blank"
       ><img
         draggable="false"
         @contextmenu.prevent
         alt="Twitter follow"
         title="Twitter follow"
-        src="https://img.shields.io/twitter/follow/ChapatizRetro?color=blue&amp;label=Twitter&amp;logo=twitter&amp;style=flat" /></a
+        src="https://img.shields.io/twitter/follow/ChimbozOfficial?color=blue&amp;label=Twitter&amp;logo=twitter&amp;style=flat"/></a
     >&nbsp;
     <a href="https://discord.gg/aYph9u8" target="_blank"
       ><img
@@ -16,7 +16,7 @@
         @contextmenu.prevent
         alt="Discord server"
         title="Discord server"
-        src="https://img.shields.io/discord/765491519536496660?label=discord&amp;logo=discord&amp;logoColor=fff" /></a
+        src="https://img.shields.io/discord/765491519536496660?label=discord&amp;logo=discord&amp;logoColor=fff"/></a
     >&nbsp;
     <a
       href="http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html"
@@ -27,7 +27,7 @@
         alt="License CeCILL v2.1"
         title="License CeCILL v2.1"
         src="https://img.shields.io/badge/license-CeCILL%20v2.1-informational"
-        @contextmenu.prevent /></a
+        @contextmenu.prevent/></a
     >&nbsp;
     <a href="https://github.com/Tigriz/chzretro-front" target="_blank"
       ><img
