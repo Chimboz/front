3e929308f0a7ac45f998f54ae4875272a82a2d22
Tigriz <tigriz@tfwno.gf>
2022-03-21 18:32:04 +0100

    Group Edit: blazon edit

--- src/view/members/group/Edit.vue
+++ src/view/members/group/Edit.vue
@@ -32,6 +32,82 @@
       </div>
     </GlobalCard>
     <br />
+    <Cabin
+      v-if="data"
+      @update-item="(name, item) => (data.blazon[name] = item)"
+      @previous-item="
+        (name) =>
+          (data.blazon[name] =
+            data.items[name][data.items[name].indexOf(data.blazon[name]) - 1])
+      "
+      @next-item="
+        (name) =>
+          (data.blazon[name] =
+            this.data.items[name][
+              data.items[name].indexOf(data.blazon[name]) + 1
+            ])
+      "
+      :data="{
+        blazon: {
+          shape: data.blazon.shape,
+          top: data.blazon.top,
+          bot: data.blazon.bot,
+          primary: data.blazon.primary,
+          secondary: data.blazon.secondary,
+        },
+        items: {
+          primary: [
+            '#444444',
+            '#CECECE',
+            '#999999',
+            '#FF0000',
+            '#CC6600',
+            '#FE9E01',
+            '#FFFF00',
+            '#ADE76B',
+            '#66CC00',
+            '#018352',
+            '#9412FE',
+            '#376DA8',
+            '#6699CC',
+            '#FE81C0',
+            '#FF3399',
+            '#E40173',
+            '#FFFFFF',
+            '#000000',
+          ],
+          top: [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+          shape: [0, 1, 2, 3, 4, 5],
+          bot: [
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
+            38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54,
+            55, 56, 57, 58,
+          ],
+          secondary: [
+            '#444444',
+            '#CECECE',
+            '#999999',
+            '#FF0000',
+            '#CC6600',
+            '#FE9E01',
+            '#FFFF00',
+            '#ADE76B',
+            '#66CC00',
+            '#018352',
+            '#9412FE',
+            '#376DA8',
+            '#6699CC',
+            '#FE81C0',
+            '#FF3399',
+            '#E40173',
+            '#FFFFFF',
+            '#000000',
+          ],
+        },
+      }"
+    />
+    <br />
     <GlobalCard color="blue" v-if="data">
       <template #header>Informations</template>
       <form>
@@ -119,8 +195,8 @@
               class="arrow green jitter"
               src="@/asset/img/arrow.svg" /></template
           >Sauver</GlobalButton
-        ></form
-      ></GlobalCard
+        >
+      </form></GlobalCard
     ><br />
     <GlobalCard color="blue" v-if="data">
       <template #header>Membres</template>
@@ -138,7 +214,8 @@
           draggable="false"
           style="cursor: var(--pointer); margin: 0 2px"
           @contextmenu.prevent
-      /></div> </GlobalCard
+        />
+      </div> </GlobalCard
     ><br />
     <GlobalCard color="blue" v-if="data">
       <template #header>Demandes</template>
@@ -155,7 +232,8 @@
           alt="Close"
           draggable="false"
           style="cursor: var(--pointer); margin: 0 2px"
-          @contextmenu.prevent /><img
+          @contextmenu.prevent
+        /><img
           @click.prevent="rejectDemand(user.id)"
           src="@/asset/img/icon/failure.svg"
           width="11"
@@ -163,7 +241,8 @@
           alt="Close"
           draggable="false"
           style="cursor: var(--pointer); margin: 0 2px"
-          @contextmenu.prevent /></div></GlobalCard
+          @contextmenu.prevent
+        /></div></GlobalCard
     ><br />
     <GlobalCard v-if="data" class="justified"
       ><img src="@/asset/img/group/bacteria.gif" style="float: left" /><b
@@ -194,7 +273,9 @@
       >/<b>{{ this.data.global.total }}</b> avec
       <b>{{ this.data.global.points }}</b> points.</GlobalCard
     ><br /><GlobalCard
-      ><a @click.prevent="this.delete()" style="color: red; cursor: var(--pointer)"
+      ><a
+        @click.prevent="this.delete()"
+        style="color: red; cursor: var(--pointer)"
         >Supprimer le groupe</a
       ></GlobalCard
     >
@@ -204,6 +285,7 @@
 <script>
 import Blazon from "@/component/blazon/Blazon.vue";
 import StrokeText from "@/component/core/StrokeText.vue";
+import Cabin from "@/component/blazon/Cabin.vue";
 import { format } from "date-fns";
 import { fr, enGB } from "date-fns/locale";
 const locales = { fr, enGB };
@@ -215,11 +297,12 @@ export default {
   name: "EditView",
   components: {
     StrokeText,
-    Blazon
+    Blazon,
+    Cabin,
   },
   data() {
     return {
-      data: null
+      data: null,
     };
   },
   async beforeRouteEnter(to, from, next) {
@@ -235,7 +318,7 @@ export default {
   methods: {
     formatDate() {
       return format(new Date(this.data.date), "PPp", {
-        locale: locales[navigator.language.split("-")[0]]
+        locale: locales[navigator.language.split("-")[0]],
       });
     },
     delete() {
@@ -249,7 +332,7 @@ export default {
     },
     rejectDemand(id) {
       console.log("Rejection du membre " + id);
-    }
+    },
   },
   metaInfo: {
     title: "section.groupedit",
@@ -257,23 +340,23 @@ export default {
       {
         name: "description",
         content:
-          "Chimboz.fr est un site pour s'amuser : tu peux tchater et te faire des amis, créer et faire évoluer ton personnage, jouer seul ou à plusieurs, fonder des groupes et même te marier !"
+          "Chimboz.fr est un site pour s'amuser : tu peux tchater et te faire des amis, créer et faire évoluer ton personnage, jouer seul ou à plusieurs, fonder des groupes et même te marier !",
       },
       {
         property: "og:title",
-        content: "Chimboz, accueil"
+        content: "Chimboz, accueil",
       },
       {
         property: "og:description",
-        content: "Chimboz, accueil"
+        content: "Chimboz, accueil",
       },
       { property: "og:site_name", content: "Chimboz" },
       { property: "og:type", content: "website" },
       { property: "og:image", content: "/announce/summer.png" },
       { property: "og:image:width", content: "192" },
-      { property: "og:image:height", content: "192" }
-    ]
-  }
+      { property: "og:image:height", content: "192" },
+    ],
+  },
 };
 </script>
 <style lang="scss" scoped>
