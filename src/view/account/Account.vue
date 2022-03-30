a0b76a0f83bfd8231e3cb86203cdb89b78536c33
Tigriz <tigriz@tfwno.gf>
2022-03-30 23:56:13 +0200

    Removing meta tags

--- src/view/account/Account.vue
+++ src/view/account/Account.vue
@@ -490,26 +490,7 @@ export default {
   },
   metaInfo: {
     title: "section.account",
-    meta: [
-      {
-        name: "description",
-        content:
-          "Chimboz.fr est un site pour s'amuser : tu peux tchater et te faire des amis, créer et faire évoluer ton personnage, jouer seul ou à plusieurs, fonder des groupes et même te marier !",
-      },
-      {
-        property: "og:title",
-        content: "Chimboz, accueil",
-      },
-      {
-        property: "og:description",
-        content: "Chimboz, accueil",
-      },
-      { property: "og:site_name", content: "Chimboz" },
-      { property: "og:type", content: "website" },
-      { property: "og:image", content: "/announce/summer.png" },
-      { property: "og:image:width", content: "192" },
-      { property: "og:image:height", content: "192" },
-    ],
+
   },
 };
 </script>
