17a9c1645a4449a59b54ed4352e62d8b5b1760bd
Tigriz <tigriz@tfwno.gf>
2022-12-17 00:30:19 +0100

    scrollbehavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  /*
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth"
+      };
+    }
+    return { top: 0, behavior: "smooth" };
+  },
+  */
   routes: [
     {
       path: "/",
