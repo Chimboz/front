54f117eac7d3d8821cb41d22a3586dbbedbabf5c
Tigriz <tigriz@tfwno.gf>
2023-01-01 00:55:04 +0100

    vue i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,13 @@ export default defineNuxtConfig({
 
   modules: ['@nuxtjs/i18n', '@pinia/nuxt'],
   css: ['@/assets/css/var.css', '@/assets/css/main.scss', '@/assets/css/fonts.css', '@/assets/css/classes.css', '@/assets/css/atomic.css'],
+  i18n: {
+    locales: [
+      { code: 'en', file: 'en.json' },
+      { code: 'fr', file: 'fr.json' }
+    ],
+    defaultLocale: "fr",
+    lazy: true,
+    langDir: "locales/",
+  }
 });
