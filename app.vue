482da60e98a2fda99c39f93f4dfd3057c4901fd4
Tigriz <tigriz@tfwno.gf>
2023-01-07 14:03:19 +0100

    bg

--- app.vue
+++ app.vue
@@ -7,39 +7,38 @@
 </template>
 
 <script setup lang="ts">
-import useAuthStore from "@/stores/auth";
-import { useHead } from "@vueuse/head";
-import favicon from "@/constants/favicon.json";
-import faviconNew from "@/constants/favicon_new.json";
-import { useI18n } from "vue-i18n";
+import useAuthStore from '@/stores/auth';
+import { useHead } from '@vueuse/head';
+import favicon from '@/constants/favicon.json';
+import faviconNew from '@/constants/favicon_new.json';
+import { useI18n } from 'vue-i18n';
 
 const notifications = computed(() => useAuthStore().notifications);
 const { t } = useI18n();
 
 useHead({
   titleTemplate: (title) =>
-    `${
-      notifications.value
-        ? notifications.value
-            .toString()
-            .replace(/[0-9]/g, (c) => "⁰¹²³⁴⁵⁶⁷⁸⁹".charAt(+c))
-        : ""
-    }Chimboz ${t(title as string)}`,
+    `${notifications.value ? notifications.value.toString().replace(/[0-9]/g, (c) => '⁰¹²³⁴⁵⁶⁷⁸⁹'.charAt(+c)) : ''}Chimboz ${t(title as string)}`,
   link: notifications.value ? faviconNew : favicon,
   meta: [
     {
-      name: "description",
+      name: 'description',
       content:
         "Chimboz est un site pour s'amuser : tu peux tchater et te faire des amis, créer et faire évoluer ton personnage, jouer seul ou à plusieurs, fonder des groupes et même te marier !",
     },
     {
-      property: "og:description",
+      property: 'og:description',
       content:
         "Chimboz.fr est un site pour s'amuser : tu peux tchater et te faire des amis, créer et faire évoluer ton personnage, jouer seul ou à plusieurs, fonder des groupes et même te marier !",
     },
     {
-      property: "og:title",
-      content: "Chimboz",
+      property: 'og:title',
+      content: 'Chimboz',
+    },
+  ],
+  style: [
+    {
+      children: `body { background-image: url(${asset(`img/navbar/background/${new Date().getHours()}.png`)}) }`,
     },
   ],
 });
