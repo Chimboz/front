62057759562ebdb6c7e6b0153c987d0a48753334
Tigriz <tigriz@tfwno.gf>
2023-02-11 23:39:21 +0100

    fix(messenger): add missing .markdown-body

--- pages/messenger/[id].vue
+++ pages/messenger/[id].vue
@@ -60,7 +60,7 @@
               </h3>
             </div>
             <span
-              class="content"
+              class="content markdown-body"
               :style="{ background: hashColor(data.user.name) }"
               v-html="$messageRender(message.content)"
             />
