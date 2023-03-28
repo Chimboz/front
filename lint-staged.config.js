12716d21d9d5bbae471f23daaa454e9dcf452484
Tigriz <tigriz@tfwno.gf>
2023-03-28 22:38:51 +0200

    feat(lint-staged): lint more files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{vue,ts,js,jsx,cjs,mjs,tsx,cts,mts}': ['eslint --fix', 'prettier --write'],
-  '*.{css,scss,json,html}': 'prettier --write',
+  '*.{css,scss,json,html,yml,md}': 'prettier --write',
   '*.svg': 'svgo --multipass',
 };
