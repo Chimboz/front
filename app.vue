9b320a2ef2db55ed02ae2c34f724f8fae8d677e4
Tigriz <tigriz@tfwno.gf>
2023-05-27 22:49:23 +0200

    chore: todo

--- app.vue
+++ app.vue
@@ -89,6 +89,7 @@ watch(i18n.locale, () =>
 // TODO check prototype pollution
 // TODO forget password page
 // TODO replace marked emoji support by extension with :nerd: + functional message copy image + check language class on code blocks + link auto detection + compare highlight.js with prismjs
+// TODO add web vitals from nuxt to a dashboard
 </script>
 <style lang="css" scoped>
 @keyframes cycleBackground {
