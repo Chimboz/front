0d73a68c1a7c259df6de67c69d3e120a4d044bbe
Tigriz <tigriz@tfwno.gf>
2023-07-28 19:53:34 +0200

    Revert "fix: vitest config"
    This reverts commit 1d02f137ac31c0fad8f1d05a2ad2aef4786d89f5.

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,5 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     globals: true,
-    coverage: {
-      provider: 'v8',
-      include: ['components/**'],
-      exclude: ['components/**/*.spec.ts'],
-      all: true,
-    },
   },
 });
