457e88e0afbe6c4b3555f45da5fed1d47a7561d4
Tigriz <tigriz@tfwno.gf>
2023-09-17 18:12:34 +0200

    fix(BacteriaMarkdown): better presentation

--- components/atoms/markdown/MarkdownBacteria.vue
+++ components/atoms/markdown/MarkdownBacteria.vue
@@ -1,21 +1,22 @@
 <template>
-  <section>
+  <section class="markdown-bacteria">
     <h1>
       Bacteria <sub>{{ move }}/{{ moves.length }}</sub>
     </h1>
-    <div class="layout">
-      <div
-        class="board pointer"
-        @click="move = minmax(move + 1, 0, moves.length)"
-        @contextmenu.prevent="move = minmax(move - 1, 0, moves.length)"
-      >
+    <Transition name="fade" mode="out-in">
+      <blockquote v-if="moves[move - 1]?.comment" :key="move">
+        <comment />
+      </blockquote>
+    </Transition>
+    <div class="layout" @wheel.prevent="scrollMove">
+      <div class="board">
         <Tooltip v-for="(tile, index) of board" :key="index" top class="cell">
           <template #tooltip
             >{{ ((index % 8) + 10).toString(36).toUpperCase()
             }}{{ Math.floor(index / 8) + 1 }}</template
           >
           <Transition name="pop" :duration="600">
-            <div :key="tile" class="tile">
+            <div :key="tile" class="tile pointer">
               <img
                 :src="asset(`img/bacteria/${tile}.svg`)"
                 :class="`type-${tile}`"
@@ -32,25 +33,38 @@
                 {{ index / 2 + 1 }}
               </td>
               <td
-                :class="`color-${1 + firstPlayer}`"
+                :class="[
+                  `color-${1 + firstPlayer}`,
+                  move === index + 1 ? 'active' : false,
+                ]"
                 class="btn-action pointer"
                 style="text-transform: uppercase"
                 @click="move = index + 1"
               >
-                {{ moves[index] }}
+                {{ moves[index].move }}
               </td>
               <td
                 v-if="moves[index + 1]"
-                :class="`color-${2 - firstPlayer}`"
+                :class="[
+                  `color-${2 - firstPlayer}`,
+                  move === index + 2 ? 'active' : false,
+                ]"
                 class="btn-action pointer"
                 style="text-transform: uppercase"
                 @click="move = index + 2"
               >
-                {{ moves[index + 1] }}
+                {{ moves[index + 1].move }}
               </td>
             </tr>
           </template>
         </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="3" width="1">
+              Vous pouvez scoller pour afficher les coups suivants et précédents
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   </section>
@@ -59,6 +73,10 @@
 import type { Tokens } from 'marked';
 
 defineProps<{ node: Tokens.Generic }>();
+
+const { $md } = useNuxtApp();
+const comment = () => $md(moves.value[move.value - 1].comment);
+
 const firstPlayer = 1;
 // prettier-ignore
 const start = [
@@ -71,7 +89,13 @@ const start = [
   0, 0, 2, 0, 0, 0, 0, 0,
   1, 0, 0, 0, 0, 0, 0, 0,
 ];
-const moves = ref<string[]>(['b2', 'b7', 'b5', 'c5', 'd4']);
+const moves = ref<{ move: string; comment: string }[]>([
+  { move: 'b2', comment: "Capturer 9 bactéries d'un coup\nCoup évident" },
+  { move: 'b7', comment: "Capturer 3 bactéries d'un coup" },
+  { move: 'b5', comment: "Capturer 2 bactéries d'un coup" },
+  { move: 'c5', comment: "Capturer 3 bactéries d'un coup" },
+  { move: 'd4', comment: "Capturer 1 bactéries d'un coup" },
+]);
 const move = ref(0);
 const board = computed(() => {
   const game = [...start];
@@ -85,9 +109,9 @@ const board = computed(() => {
       xFrom?: number;
       yFrom?: number;
     } = {
-      explicit: moves.value[i],
-      x: moves.value[i].charCodeAt(moves.value[i].length - 2) - 97,
-      y: +moves.value[i][moves.value[i].length - 1] - 1,
+      explicit: moves.value[i].move,
+      x: moves.value[i].move.charCodeAt(moves.value[i].move.length - 2) - 97,
+      y: +moves.value[i].move[moves.value[i].move.length - 1] - 1,
     };
 
     const implicitJump = game.map((value, index) => {
@@ -108,7 +132,7 @@ const board = computed(() => {
       );
     });
 
-    if (/[a-zA-Z]\dj[a-zA-Z]\d/.test(moves.value[i])) {
+    if (/[a-zA-Z]\dj[a-zA-Z]\d/.test(moves.value[i].move)) {
       m.xFrom = m.explicit.charCodeAt(0) - 97;
       m.yFrom = +m.explicit[1] - 1;
       game[m.xFrom + 8 * m.yFrom] = 0;
@@ -133,78 +157,105 @@ const board = computed(() => {
   }
   return game;
 });
+
+function scrollMove(event: any) {
+  if (event.deltaY > 0)
+    move.value = minmax(move.value + 1, 0, moves.value.length);
+  if (event.deltaY < 0)
+    move.value = minmax(move.value - 1, 0, moves.value.length);
+}
 </script>
 <style lang="scss">
-.layout {
-  display: flex;
-  .board {
-    display: grid;
-    flex: 1;
-    aspect-ratio: 1.73931808373;
-    grid: repeat(8, 1fr) / repeat(8, 1fr);
-    background-image: url(@/assets/img/bacteria/floor.svg);
-    background-size: contain;
-    background-repeat: no-repeat;
-    padding: 0 6% 5% 6%;
-    .cell {
-      display: flex;
-      justify-content: center;
-      align-items: flex-end;
-      user-select: none;
-      .tile {
-        position: absolute;
-        width: 100%;
-        text-align: center;
-        img {
-          &.type-0,
-          &.type-3 {
-            transition: 0.4s;
-            width: 90%;
-          }
-          &.type-1,
-          &.type-2 {
-            width: 66%;
-          }
-        }
-      }
-      &::after {
-        content: '';
-        border-radius: 100%;
-        transition: var(--duration);
-        width: 100%;
-        position: absolute;
-        height: 60%;
-        z-index: -1;
-      }
-
-      &:hover {
-        z-index: 2;
-        &::after {
-          background: #fff;
-        }
-      }
-    }
+.markdown-bacteria {
+  h1 {
+    margin-top: 0;
   }
-  .moves {
-    .color-1 {
-      border-color: var(--main-button-red);
-      color: var(--dark-button-red);
-      border-radius: 0;
-      background: linear-gradient(
-        to bottom,
-        var(--light),
-        var(--main-button-red)
-      );
+  .layout {
+    display: flex;
+    cursor: nw-resize;
+    .board {
+      display: grid;
+      flex: 1;
+      aspect-ratio: 1.73931808373;
+      grid: repeat(8, 1fr) / repeat(8, 1fr);
+      background-image: url(@/assets/img/bacteria/floor.svg);
+      background-size: contain;
+      background-repeat: no-repeat;
+      padding: 0 6% 5% 6%;
+      .cell {
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        user-select: none;
+        .tile {
+          position: absolute;
+          width: 100%;
+          text-align: center;
+          img {
+            &.type-0,
+            &.type-3 {
+              transition: 0.4s;
+              width: 90%;
+            }
+            &.type-1,
+            &.type-2 {
+              width: 66%;
+            }
+          }
+        }
+        &::after {
+          content: '';
+          border-radius: 100%;
+          transition: var(--duration);
+          width: 100%;
+          position: absolute;
+          height: 60%;
+          z-index: -1;
+        }
+
+        &:hover {
+          z-index: 2;
+          &::after {
+            background: #fff;
+          }
+        }
+      }
     }
-    .color-2 {
-      border-radius: 0;
-      border-color: var(--main-button-green);
-      color: var(--dark-button-green);
-      background: linear-gradient(
-        to bottom,
-        var(--light),
-        var(--main-button-green)
-      );
+    .moves {
+      .color-1 {
+        border-color: var(--main-button-red);
+        color: var(--dark-button-red);
+        border-radius: 0;
+        background: linear-gradient(
+          to bottom,
+          var(--light),
+          var(--main-button-red)
+        );
+        &.active {
+          background: linear-gradient(
+            to top,
+            var(--light),
+            var(--main-button-red)
+          );
+        }
+      }
+      .color-2 {
+        border-radius: 0;
+        border-color: var(--main-button-green);
+        color: var(--dark-button-green);
+        background: linear-gradient(
+          to bottom,
+          var(--light),
+          var(--main-button-green)
+        );
+        &.active {
+          background: linear-gradient(
+            to top,
+            var(--light),
+            var(--main-button-green)
+          );
+        }
+      }
     }
   }
 }
